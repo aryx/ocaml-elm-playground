2ff563a4f27fbf7c44ea3985f2eb8e5285f18c45
pad <yoann.padioleau@gmail.com>
2020-11-02 14:09:55 +0100

    * lib/Playground.ml: progress

--- examples/Smiley.ml
+++ examples/Smiley.ml
@@ -1,6 +1,14 @@
 open Playground
 
-let main =
+let app =
   picture [
     circle lightYellow 200.;
   ]
+
+
+open Js_browser
+
+let run () = Vdom_blit.run app |> Vdom_blit.dom |> Element.append_child (Document.body document)
+let () = Window.set_onload window run
+
+  
--- lib/Playground.ml
+++ lib/Playground.ml
@@ -1,10 +1,48 @@
 open Common
+module V = Vdom
+
+(* in Core.elm *)
 
 type number = float
+let (/) = (/.)
+let (+) = (+.)
+let (-) = (-.)
+let ( * ) = ( *. )
+
+module Platform = struct
+type ('flags, 'model, 'msg) program =
+  ('model, 'msg) V.app
+end
+
+module Cmd = struct
+type 'msg t = 'msg V.Cmd.t
+let none = V.Cmd.Batch []
+end
+
+
+module Browser = struct
+type 'msg document = 'msg V.vdom
+
+type ('flags, 'model, 'msg) app = {
+  init: 'flags -> ('model * 'msg V.Cmd.t);
+  view: 'model -> 'msg V.vdom;
+  update: 'msg -> 'model -> ('model * 'msg V.Cmd.t);
+  (* subscriptions: 'model -> 'msg V.Sub?? *)
+}
+
+let (document: ('flags, 'model, 'msg) app -> 
+               ((*'flags,*) 'model, 'msg) V.app) 
+ = fun { init; view; update } ->
+  V.app ~init:(init ()) ~view ~update:(fun model msg -> update msg model) ()
+end
+
+(* in ???.elm *)
 
 type color = string
 
 let hex s = s
+
+(* in Playground.elm *)
 
 let lightYellow = hex "fce94f"
 
@@ -36,13 +74,30 @@ type screen = {
   bottom: number;
 }
 
+let (to_screen: number -> number -> screen) = fun width height ->
+  { width; 
+    height; 
+    top = height / 2.;
+    left = (-. width) / 2.;
+    right = width / 2.;
+    bottom = (-. height) / 2.;
+  }
+    
 
-
-(* in Platform.elm *)
-type ('flags, 'model, 'msg) program =
-  Program
-
-
-let (picture: shape list -> (unit, screen, (int * int)) program) = 
+let (picture: shape list -> (unit, screen, (int * int)) Platform.program) = 
  fun _shapes ->
-  raise Todo
+  let init () = 
+      to_screen 600. 600., Cmd.none
+  in
+  let view _screen = 
+      raise Todo
+  in
+  let update (width, height) _model = 
+     to_screen (float_of_int width) (float_of_int height), Cmd.none
+  in
+  let _subscriptions _ =
+      raise Todo
+  in
+
+  Browser.document { Browser. init; view; update }
+    
