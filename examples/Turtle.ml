ed98702ca21d1f1df6950038ed3cfe9c457c32be
pad <yoann.padioleau@gmail.com>
2020-11-30 14:57:53 +0100

    * examples/Turtle.ml: new example

---
+++ examples/Turtle.ml
@@ -0,0 +1,29 @@
+open Playground
+open Basics
+
+type turtle = {
+  x: number;
+  y: number;
+  angle: number;
+}
+
+let initial_turtle = { x = 0.; y = 0.; angle = 0. }
+
+let view computer turtle =
+  [ rectangle blue computer.screen.width computer.screen.height;
+    rectangle black 96. 96.
+    |> move turtle.x turtle.y
+    |> rotate turtle.angle
+  ]
+
+let update computer turtle =
+  { x = turtle.x + 
+      to_y computer.keyboard * cos (degrees_to_radians turtle.angle);
+    y = turtle.y +
+      to_y computer.keyboard * sin (degrees_to_radians turtle.angle);
+    angle = turtle.angle - to_x computer.keyboard
+  }
+
+let app = game view update initial_turtle
+
+let main = Playground_platform.run_app app
